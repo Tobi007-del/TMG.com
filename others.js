5c3bd900aba3c37aa2f92a99877068fca56a069a
Tobi-007del <tobioketade007@gmail.com>
2024-08-02 22:40:48 +0100

    testing live chat service

--- others.js
+++ others.js
@@ -735,3 +735,15 @@ function openPanel(panelName, hiddenPanelName){
     document.getElementById(hiddenPanelName).classList.add('open');
     document.getElementById(hiddenPanelName).classList.remove('close');
 }
+
+//Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66aca05b32dca6db2cb91c87/1i4954q1f';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+//End of Tawk.to Script
